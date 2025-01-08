358154f83a1a0eb767a855cdf2ad8579992dd7e6
1Jeferson <jefersonnanet.95@gmail.com>
2025-01-08 18:13:58 -0300

    refactor: update ErrorMessage component to use destructuring for props

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -1,4 +1,4 @@
-const ErrorMessage = (message?: string) => {
+const ErrorMessage = ({ message }: { message?: string }) => {
   if (!message) return null;
 
   return <p className='text-red-500 text-sm mt-2'>{message}</p>;
