4b80d24944da6515fae0799e972acd5c0adda9df
1Jeferson <jefersonnanet.95@gmail.com>
2025-01-09 12:53:14 -0300

    feat: add login validation with Zod and react-hook-form

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,81 +1,71 @@
-import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
+import { ValidationLoginSchema } from '../../schemas';
 import Button from '../../components/button';
 import ErrorMessage from '../../components/message';
-import { twMerge } from 'tailwind-merge';
+import { useState } from 'react';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [errors, setErrors] = useState<{ email?: string; password?: string }>({
-    email: '',
-    password: '',
+  const [error, setError] = useState<string | null>(null);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    resolver: zodResolver(ValidationLoginSchema),
   });
 
-  const validateForm = () => {
-    const newErrors: { email?: string; password?: string } = {};
+  const onSubmit = async (data: LoginData) => {
+    setError(null);
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/.test(password);
-
-    if (!email) {
-      newErrors.email = 'E-mail é obrigatório.';
-    } else if (!emailRegex) {
-      newErrors.email = 'Por favor, insira um e-mail válido.';
-    }
-
-    if (!password) {
-      newErrors.password = 'Senha é obrigatória.';
-    } else if (!passwordRegex) {
-      newErrors.password = 'A senha deve conter pelo menos um número e uma letra, e ter no mínimo 7 caracteres.';
-    }
-
-    setErrors(newErrors);
-
-    return Object.keys(newErrors).length === 0;
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (validateForm()) {
-      console.log('Formulário enviado com sucesso!', { email, password });
+    try {
+      console.log(data);
+    } catch (err) {
+      setError('Falha ao fazer login. Tente novamente.');
+    } finally {
     }
   };
 
   return (
     <div className='flex flex-col justify-center items-center gap-8 mt-8 p-10 sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-[40%] m-auto rounded-2xl'>
       <div className='flex flex-col justify-center items-center w-full h-1/2 p-8 rounded-2xl'>
-        <h2 className='text-2xl text-center font-semibold py-4'>Bem-vindo(a) a Instaq</h2>
+        <h2 className='text-2xl text-center font-semibold py-4 text-green-950'>Bem-vindo(a) a Instaq</h2>
 
-        <form className='flex flex-col justify-center items-center w-full gap-6' onSubmit={handleSubmit}>
+        <form className='flex flex-col justify-center items-center w-full gap-6' onSubmit={handleSubmit(onSubmit)}>
           <div className='w-full'>
             <input
               type='email'
               placeholder='E-mail'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              {...register('email')}
               className={twMerge(
                 'p-3 rounded-2xl w-full border-2 text-lg focus:outline-none',
                 errors.email ? 'border-red-500' : 'border-indigo-500',
               )}
             />
-
-            <ErrorMessage message={errors.email} />
+            <ErrorMessage message={errors.email?.message} />
           </div>
 
           <div className='w-full'>
             <input
               type='password'
               placeholder='Senha'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register('password')}
               className={twMerge(
                 'p-3 rounded-2xl w-full border-2 text-lg focus:outline-none',
                 errors.password ? 'border-red-500' : 'border-indigo-500',
               )}
             />
-
-            <ErrorMessage message={errors.password} />
+            <ErrorMessage message={errors.password?.message} />
           </div>
+
+          {error && <ErrorMessage message={error} />}
 
           <Button>Entrar</Button>
         </form>
---
+++ src/schemas/index.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const ValidationLoginSchema = z.object({
+  email: z
+    .string()
+    .nonempty({ message: 'E-mail é obrigatório.' })
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: 'Por favor, insira um e-mail válido.' }),
+  password: z
+    .string()
+    .nonempty({ message: 'Senha é obrigatória.' })
+    .min(7, { message: 'A senha deve conter pelo menos 7 caracteres.' })
+    .regex(/(?=.*[a-zA-Z])(?=.*\d)/, { message: 'A senha deve conter pelo menos um número e uma letra.' }),
+});
